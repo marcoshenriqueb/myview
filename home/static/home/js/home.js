cebe4f40a0fe9d477678b5e496b25e71c4e33931
marcos Almeida <marcoshenriqueb@gmail.com>
2017-05-10 00:22:53 -0300

    Top section

--- home/static/home/js/home.js
+++ home/static/home/js/home.js
@@ -5,4 +5,21 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('mobile-menu').addEventListener('click', () => {
         document.querySelector('header.header').classList.toggle('active');
     });
+
+
+    const adjustVideo = () => {
+        const w = window.innerWidth;
+        if (w > 768) {
+            const vRatio = v.videoHeight / v.videoWidth;
+            top.style.height = (vRatio * w) + 'px';
+        } else {
+            top.style.height = 'calc(100vh - 100px)';
+        }
+    }
+    const top = document.querySelector('.top');
+    const v = document.getElementsByTagName('video')[0];
+    v.addEventListener('loadeddata', () => {
+        adjustVideo();
+    });
+    window.onresize = adjustVideo;
 });
