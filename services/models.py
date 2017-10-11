bd9db402e01b13e0b819aac0dc6064db6f9a4f52
Marcos Almeida <marcoshenriqueb@gmail.com>
2017-10-11 20:30:28 -0300

    Link on solutions

--- services/models.py
+++ services/models.py
@@ -20,6 +20,7 @@ class Service(models.Model):
 class Solution(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name="Usuário")
     name = models.CharField("Nome", max_length=255)
+    link = models.CharField("Link Vimeo", max_length=255, null=True, blank=True)
     description = models.TextField("Descrição", null=True, blank=True)
     photo = models.ImageField("Foto", upload_to='solutionsphotos/%Y/%m/%d/', max_length=255, null=True, blank=True)
     home = models.BooleanField("Colocar na home", default=False)
